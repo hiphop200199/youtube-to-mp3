bd68bb493473701330d3fc71a643b3c4e8d67c99
Eric Tsai <hiphop200199@gmail.com>
2023-03-28 12:48:38 +0800

    file uploads

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/youtube-download/',
+  base:'/youtube-to-mp3/',
   plugins: [react()],
 })
